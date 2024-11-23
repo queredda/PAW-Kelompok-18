d5658bce32756dbaa6693acab79e56df7d4cbdc2
queredda <mahsaqueredabahjah@mail.ugm.ac.id>
2024-11-23 21:32:26 +0700

    feat: add inventory types

---
+++ src/types/inventory.ts
@@ -0,0 +1,41 @@
+ interface InventoryItem {
+  id: string
+  name: string
+  category: string
+  location: string
+  quantity: number
+  image: string
+  status: 'Available' | 'Borrowed'
+}
+
+ interface BorrowedItem {
+  trackingId: string
+  product: {
+    name: string
+    image: string
+  }
+  customer: string
+  date: string
+  amount: number
+  paymentMode: string
+  status: string
+}
+
+ interface InventoryStats {
+  totalItems: number
+  goodCondition: number
+  badCondition: number
+}
+
+interface BorrowedStats {
+  totalRequests: number
+  totalBorrowed: number
+  totalReturned: number
+}
+
+interface NavbarProps {
+  menu: string;
+  path: string;
+}
+
+export type { NavbarProps, InventoryItem, BorrowedItem, InventoryStats, BorrowedStats };
