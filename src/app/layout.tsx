23917263b26341a4bb9240c71c4942471ecd2e2c
queredda <mahsaqueredabahjah@mail.ugm.ac.id>
2024-11-24 23:04:58 +0700

    feat: update layouting page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,14 @@
 
 import './globals.css';
 import React from 'react';
-import { poppins } from "@/app/font";
-import dynamic from "next/dynamic";
-import { Toaster } from "@/components/ui/toaster";
+import { poppins } from '@/app/font';
+import dynamic from 'next/dynamic';
+import { Toaster } from '@/components/ui/toaster';
 
-
-const Navbar = dynamic(() => import("@/components/global/Navbar"), {
+const Navbar = dynamic(() => import('@/components/global/Navbar'), {
   ssr: false,
 });
-const Footer = dynamic(() => import("@/components/global/Footer"), {
+const Footer = dynamic(() => import('@/components/global/Footer'), {
   ssr: false,
 });
 
@@ -22,12 +21,12 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body className={`${poppins.variable}`}>
-          <Navbar />
-          <Toaster />
-          <main className={`py-[100px] md:px-[120px] bg-Background-A`}>
-            {children}
+        <Navbar />
+        <Toaster />
+        <main className={`py-[100px] md:px-[120px] bg-Background-A overflow-hidden`}>
+          {children}
         </main>
-          <Footer />
+        <Footer />
       </body>
     </html>
   );
