66876eb3c46a2be0e638cf6c936fd1649f00d68c
queredda <mahsaqueredabahjah@mail.ugm.ac.id>
2024-11-26 14:58:12 +0700

    feat: add provider to handle auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { poppins } from '@/app/font';
 import dynamic from 'next/dynamic';
 import { Toaster } from '@/components/ui/toaster';
+import { Providers } from "./providers";
 
 const Navbar = dynamic(() => import('@/components/global/Navbar'), {
   ssr: false,
@@ -21,12 +22,14 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body className={`${poppins.variable}`}>
-        <Navbar />
-        <Toaster />
-        <main className={`py-[100px] md:px-[120px] bg-Background-A overflow-hidden`}>
-          {children}
-        </main>
-        <Footer />
+        <Providers>
+          <Navbar />
+          <Toaster />
+          <main className={`py-[150px] md:px-[120px] bg-Background-A overflow-hidden`}>
+            {children}
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
