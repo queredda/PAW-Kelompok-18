55698cf1c656f568d20483f6b40ff9dc73c55b35
farreladriann <muhamadfarreladrian@mail.ugm.ac.id>
2024-11-27 17:45:02 +0700

    feat: implement dynamic stats fetching and update StatsCards component

--- src/types/inventory.ts
+++ src/types/inventory.ts
@@ -1,40 +1,8 @@
- interface InventoryItem {
-  id: string
-  name: string
-  category: string
-  location: string
-  quantity: number
-  image: string
-  status: 'Available' | 'Borrowed'
+// types/inventory.ts
+export interface Stats {
+  totalItem: number;
+  totalItemBaik: number;
+  totalItemRusak: number;
+  totalItemDipinjam: number;
+  totalPermintaanPeminjaman: number;
 }
-
- interface BorrowedItem {
-  trackingId: string
-  product: {
-    name: string
-    image: string
-  }
-  customer: string
-  date: string
-  amount: number
-  status: string
-}
-
- interface InventoryStats {
-  totalItems: number
-  goodCondition: number
-  badCondition: number
-}
-
-interface BorrowedStats {
-  totalRequests: number
-  totalBorrowed: number
-  totalReturned: number
-}
-
-interface NavbarProps {
-  menu: string;
-  path: string;
-}
-
-export type { NavbarProps, InventoryItem, BorrowedItem, InventoryStats, BorrowedStats };
